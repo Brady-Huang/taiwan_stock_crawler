0c9990c28f9619adcc4832cd2ef07b420150e4fc
Brady <brady120915@gmail.com>
2026-07-11 21:59:41 +0800

    fix: remove hardcoded credentials, use env variables

--- init.sql
+++ init.sql
@@ -1,8 +1,6 @@
 CREATE DATABASE IF NOT EXISTS airflow_metadata;
 CREATE DATABASE IF NOT EXISTS taiwan_stock;
 
-CREATE USER IF NOT EXISTS 'airflow_user'@'%' IDENTIFIED BY 'demo1234';
-GRANT ALL PRIVILEGES ON taiwan_stock.* TO 'airflow_user'@'%';
 GRANT ALL PRIVILEGES ON airflow_metadata.* TO 'airflow_user'@'%';
 
 FLUSH PRIVILEGES;
